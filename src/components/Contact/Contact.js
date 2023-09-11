05f8cdc881ef8f8e9bd8a0fa910ef28ddbfcd8db
sachitha-bandara <sachitha.chamara@gmail.com>
2023-09-11 12:17:11 +1000

    resolve build errors for dep

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import Particle from "../Particle";
-import Github from "./Github";
-import Techstack from "./Techstack";
-import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
-
 function Contact() {
   return (
     <Container fluid className="about-section">
