9103c9eda3f417f186cd8bd5d3f69c2124f0a686
sachitha-bandara <sachitha.chamara@gmail.com>
2023-09-18 11:56:49 +1000

    rectify spellings

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -147,7 +147,7 @@ function Home2() {
               Workshop registration will open soon!
               <br />
               <br />
-              <strong>o facilitate maximum participation, all attendees are invited to present a poster at the workshop. More information about the poster abstract submission will be available soon.</strong>
+              <strong>To facilitate maximum participation, all attendees are invited to present a poster at the workshop. More information about the poster abstract submission will be available soon.</strong>
               <br/>
               
             </p>
