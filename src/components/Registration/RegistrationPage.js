52e80b84d7dd295645066f8c30d3196d139cef40
sachitha-bandara <sachitha.chamara@gmail.com>
2023-10-08 11:43:04 +1100

    publish registration

--- src/components/Registration/RegistrationPage.js
+++ src/components/Registration/RegistrationPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import PDFInstructions from "../../Assets/Instructions.pdf";
+import PDFInstructions from "../../Assets/Instructions_new.pdf";
 
 function RegistrationPage(){
     
