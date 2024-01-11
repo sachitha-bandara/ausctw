122aea50c3e4c9f2e5131b8d60743e7cd04afdf6
sachitha-bandara <sachitha.chamara@gmail.com>
2024-01-11 17:25:23 +1100

    detailed program added

---
+++ src/components/Program/CardForSpeaker.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Card.css'; // Create a separate CSS file for styling
+
+function CardForSpeaker ({name,title,abstract,bio, talk, leftColumnRatio, rightColumnRatio}) {
+  return (
+    <div className="card">
+      <div className="cardTitle"> <span className='talkType'>{talk}</span> : {name}</div>
+      <div className="cardContent">
+        <div className="leftColumn" style={{ flex: leftColumnRatio }}>
+          <div className="columnTitle">Title : {title}</div>
+          <div className="columnContent">
+            <p> <span className='boldText'>Abstract</span> : {abstract}</p>
+          </div>
+        </div>
+        <div className="rightColumn" style={{ flex: rightColumnRatio }}>
+          {/* <div className="columnTitle">Right Column Title</div> */}
+          <div className="columnContent">
+            <p><span className='boldText'>Bio</span> : {bio}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardForSpeaker;
--- src/components/Program/Program.js
+++ src/components/Program/Program.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 // import Particle from "../Particle";
 import Schedule from "./Schedule";
 import BannerOther from "../Home/BannerOther";
+import Talks from "./Talks";
+import PDFTalkDetails from "../../Assets/TalkDetails.pdf";
 
 function Program() {
   return (
@@ -11,6 +13,10 @@ function Program() {
         <BannerOther mainText="Program"/>
       </Container>
       <Schedule />
+      <Container>
+        <div style={{fontWeight: "bold"}}>Click <a href={PDFTalkDetails} target="_blank">here</a> to download the detailed program.</div>
+      </Container>
+      <Talks />
     </section>
   );
 }
